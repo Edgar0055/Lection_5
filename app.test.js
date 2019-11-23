f5b2c9e862b1083e94371e9dcbf6d9245f0750a9
Edgar Rostomian <e.rostomian13@gmail.com>
2019-11-23 16:37:15 +0200

    add new test

--- app.test.js
+++ app.test.js
@@ -20,3 +20,13 @@ describe ( 'app.test 2', () => {
         expect( 2 ).not.toBe( 1 );
     } );
 } );
+
+describe ( 'app.test 3', () => {
+    test( 'test toBe success', ( ) => {
+        expect( 1 ).toBe( 1 );
+    } );
+    
+    test( 'test not toBe success', ( ) => {
+        expect( 2 ).not.toBe( 1 );
+    } );
+} );
