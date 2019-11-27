163bd954e97d9b59bd7c28aa2c03bee1ee80e64d
Edgar0055 <51047440+Edgar0055@users.noreply.github.com>
2019-11-27 23:39:43 +0200

    Node 147 (#10)
    NODE-147 done

---
+++ docs/swagger-config.js
@@ -0,0 +1,58 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Blog API',
+        version: '1.0.0',
+        description: 'Description example',
+        termsOfService: 'http://exapmle.com/terms/',
+        constact: {
+            name: 'API Support',
+            url: 'http://www.example.com/support/',
+            email: 'supper@examle.com',
+        },
+        license: {
+            name: 'Apache 2.0',
+            url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
+        },
+    },
+    basePath: '/',
+    components: {},
+    security: [],
+};
+const swaggerUiOptions = {
+    customCss: '.swagger-ui .topbar { display: none; }',
+    swaggerOptions: {
+        filter: true,
+        requestInterceptor: ( req ) => {
+            try {
+                const regex = /XSRF-TOKEN=(.[^;]*)/ig;
+                const match = regex.exec( document.cookie );
+                req.headers[ 'x-csrf-token' ] = match[1];    
+            } catch ( error ) {}
+            return req;
+        },
+    },
+};
+
+module.exports = ( app ) => app.use(
+    '/docs',
+    ( req, res, next ) => {
+        try {
+            req.swaggerDoc = swaggerJsDoc( {
+                swaggerDefinition,
+                apis: [
+                    `${ __dirname }/components/**/*.yaml`,
+                    `${ __dirname }/paths/**/*.yaml`,
+                ]
+            } );
+            next();    
+        } catch ( error ) {
+            next( error );
+        }
+    },
+    swaggerUi.serve,
+    swaggerUi.setup( null, swaggerUiOptions ),
+);
